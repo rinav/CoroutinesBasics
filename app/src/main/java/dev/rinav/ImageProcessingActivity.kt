861d69577db913f37061c91d571ebcd70c3745d2
Rinav Gangar <rinav4all@gmail.com>
2020-05-11 01:18:59 +0530

    Refactor

--- app/src/main/java/dev/rinav/ImageProcessingActivity.kt
+++ app/src/main/java/dev/rinav/ImageProcessingActivity.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_image_processing.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.net.URL
 
-class MainActivity : AppCompatActivity() {
+class ImageProcessingActivity : AppCompatActivity() {
 
     private val imageUrl =
         "https://images.unsplash.com/photo-1588605840431-a6d0f9af60b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80"
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_image_processing)
 
         coroutineScope.launch {
 
